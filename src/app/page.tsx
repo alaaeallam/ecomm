20f488931cbe52b24bfb8dd92abf5deefc833597
alaaeallam <alaaeallam@gmail.com>
2025-01-04 20:08:21 +0200

    add theme and fonts,shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-
+import ThemeToggle from "@/components/shared/theme-toggle";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="font-bold text-blue-500">lolo</h1>
+    <div className="p-5">
+      <div className="w-100 flex justify-end">
+        <ThemeToggle/>
+      </div>
+      <h1 className="font-bold text-blue-500">no</h1>
+      <h1 className=" text-blue-500 font-barlow">yes</h1>
+      
     </div>
   );
 }
